5cd90db366e933a5efd4f3960e03a7665f1cd958
jedeop <jedeop@jedeop.dev>
2023-01-27 15:11:26 +0900

    load project with minimize mode

---
+++ app/src/assets/project.js
@@ -0,0 +1,240 @@
+export const project = {
+  "objects": [
+    {
+      "id": "tvis",
+      "name": "소피",
+      "script": "[[{\"id\":\"7p1i\",\"x\":50,\"y\":30,\"type\":\"when_run_button_click\",\"params\":[null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},{\"id\":\"bzrs\",\"x\":0,\"y\":0,\"type\":\"repeat_inf\",\"params\":[null,null],\"statements\":[[{\"id\":\"okej\",\"x\":0,\"y\":0,\"type\":\"locate_xy\",\"params\":[{\"id\":\"q00z\",\"x\":0,\"y\":0,\"type\":\"calc_basic\",\"params\":[{\"id\":\"du3z\",\"x\":0,\"y\":0,\"type\":\"calc_basic\",\"params\":[{\"id\":\"bnyt\",\"x\":0,\"y\":0,\"type\":\"calc_operation\",\"params\":[null,{\"id\":\"bqu7\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"derv\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null,\"cos\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},\"MULTI\",{\"id\":\"hrsu\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"sn65\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},\"PLUS\",{\"id\":\"9l2r\",\"x\":0,\"y\":0,\"type\":\"coordinate_object\",\"params\":[null,\"7y0y\",null,\"x\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},{\"id\":\"hcpm\",\"x\":0,\"y\":0,\"type\":\"calc_basic\",\"params\":[{\"id\":\"fw79\",\"x\":0,\"y\":0,\"type\":\"calc_basic\",\"params\":[{\"id\":\"e6ot\",\"x\":0,\"y\":0,\"type\":\"calc_operation\",\"params\":[null,{\"id\":\"2wjy\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"derv\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null,\"sin\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},\"MULTI\",{\"id\":\"tld7\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"sn65\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},\"PLUS\",{\"id\":\"d00a\",\"x\":0,\"y\":0,\"type\":\"coordinate_object\",\"params\":[null,\"7y0y\",null,\"y\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},{\"id\":\"p5qt\",\"x\":0,\"y\":0,\"type\":\"set_variable\",\"params\":[\"derv\",{\"id\":\"cceg\",\"x\":0,\"y\":0,\"type\":\"quotient_and_mod\",\"params\":[null,{\"id\":\"w7x8\",\"x\":0,\"y\":0,\"type\":\"calc_basic\",\"params\":[{\"id\":\"mcxd\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"derv\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},\"PLUS\",{\"id\":\"c2y8\",\"x\":0,\"y\":0,\"type\":\"get_variable\",\"params\":[\"f6gr\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null,{\"id\":\"39f7\",\"x\":0,\"y\":0,\"type\":\"text\",\"params\":[\"360\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null,\"MOD\"],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}]],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}]]",
+      "objectType": "sprite",
+      "rotateMethod": "free",
+      "scene": "7dwq",
+      "sprite": {
+        "pictures": [
+          {
+            "id": "b7sw",
+            "dimension": {
+              "width": 303,
+              "height": 214,
+              "scaleX": 0.17986221459325952,
+              "scaleY": 0.17986221459325943
+            },
+            "filename": "488f15d61d268d972e466dacf8ae4d53",
+            "name": "소피_1",
+            "scale": 100,
+            "imageType": "png"
+          },
+          {
+            "id": "k4ll",
+            "dimension": {
+              "width": 303,
+              "height": 214
+            },
+            "filename": "19650728902d96932a6415e44add73b7",
+            "name": "소피_2",
+            "scale": 100,
+            "imageType": "png"
+          },
+          {
+            "id": "q6o9",
+            "dimension": {
+              "width": 303,
+              "height": 214
+            },
+            "filename": "52a7e41fbf2207ba218da5f01eb4554f",
+            "name": "소피_3",
+            "scale": 100,
+            "imageType": "png"
+          },
+          {
+            "id": "7yz1",
+            "dimension": {
+              "width": 303,
+              "height": 214
+            },
+            "filename": "db36df62a2e380f7035a158970dc3b5a",
+            "name": "소피_4",
+            "scale": 100,
+            "imageType": "png"
+          },
+          {
+            "id": "f5wv",
+            "dimension": {
+              "width": 303,
+              "height": 214
+            },
+            "filename": "bcd90210bf4aad13f040a7cbd61708b4",
+            "name": "소피_5",
+            "scale": 100,
+            "imageType": "png"
+          }
+        ],
+        "sounds": []
+      },
+      "selectedPictureId": "b7sw",
+      "lock": false,
+      "entity": {
+        "x": 31.36,
+        "y": -22.15,
+        "regX": 151.5,
+        "regY": 107,
+        "scaleX": 0.17986221459325952,
+        "scaleY": 0.17986221459325943,
+        "rotation": 0,
+        "direction": 90,
+        "width": 303,
+        "height": 214,
+        "font": "undefinedpx ",
+        "visible": true
+      }
+    },
+    {
+      "id": "7y0y",
+      "name": "엔트리봇",
+      "script": "[[{\"id\":\"qefb\",\"x\":50,\"y\":30,\"type\":\"when_run_button_click\",\"params\":[null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]},{\"id\":\"bhb6\",\"x\":0,\"y\":0,\"type\":\"repeat_inf\",\"params\":[null,null],\"statements\":[[{\"id\":\"tjkx\",\"x\":0,\"y\":0,\"type\":\"locate\",\"params\":[\"mouse\",null],\"statements\":[],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}]],\"movable\":null,\"deletable\":1,\"emphasized\":false,\"readOnly\":null,\"copyable\":true,\"assemble\":true,\"extensions\":[]}]]",
+      "objectType": "sprite",
+      "rotateMethod": "free",
+      "scene": "7dwq",
+      "sprite": {
+        "pictures": [
+          {
+            "id": "vx80",
+            "dimension": {
+              "width": 284,
+              "height": 350,
+              "scaleX": 0.19060059523698575,
+              "scaleY": 0.1906005952369856
+            },
+            "fileurl": "/lib/entry-js/images/media/entrybot1.png",
+            "name": "엔트리봇_걷기1",
+            "scale": 100,
+            "imageType": "png"
+          },
+          {
+            "id": "4t48",
+            "dimension": {
+              "width": 284,
+              "height": 350
+            },
+            "fileurl": "/lib/entry-js/images/media/entrybot2.png",
+            "name": "엔트리봇_걷기2",
+            "scale": 100,
+            "imageType": "png"
+          }
+        ],
+        "sounds": [
+          {
+            "duration": 1.3,
+            "ext": ".mp3",
+            "id": "8el5",
+            "fileurl": "/lib/entry-js/images/media/bark.mp3",
+            "name": "강아지 짖는 소리"
+          }
+        ]
+      },
+      "selectedPictureId": "vx80",
+      "lock": false,
+      "entity": {
+        "x": 17.73,
+        "y": -21.85,
+        "regX": 142,
+        "regY": 175,
+        "scaleX": 0.19060059523698575,
+        "scaleY": 0.1906005952369856,
+        "rotation": 0,
+        "direction": 90,
+        "width": 284,
+        "height": 350,
+        "font": "undefinedpx ",
+        "visible": true
+      }
+    }
+  ],
+  "scenes": [
+    {
+      "id": "7dwq",
+      "name": "ver1"
+    }
+  ],
+  "variables": [
+    {
+      "name": "theta",
+      "id": "derv",
+      "visible": true,
+      "value": 0,
+      "variableType": "variable",
+      "isCloud": false,
+      "isRealTime": false,
+      "cloudDate": false,
+      "object": null,
+      "x": 139.31263858093126,
+      "y": -97.54976679946957
+    },
+    {
+      "name": "Δrotation",
+      "id": "f6gr",
+      "visible": true,
+      "value": 5,
+      "variableType": "slide",
+      "isCloud": false,
+      "isRealTime": false,
+      "cloudDate": false,
+      "object": null,
+      "x": -230,
+      "y": -31.234281768170135,
+      "minValue": "0.0",
+      "maxValue": "10.0"
+    },
+    {
+      "name": "r",
+      "id": "sn65",
+      "visible": true,
+      "value": 70,
+      "variableType": "slide",
+      "isCloud": false,
+      "isRealTime": false,
+      "cloudDate": false,
+      "object": null,
+      "x": -230,
+      "y": -77,
+      "minValue": "10",
+      "maxValue": "150"
+    },
+    {
+      "name": "초시계",
+      "id": "brih",
+      "visible": false,
+      "value": 0,
+      "variableType": "timer",
+      "isCloud": false,
+      "isRealTime": false,
+      "cloudDate": false,
+      "object": null,
+      "x": 134,
+      "y": -70
+    },
+    {
+      "name": " 대답 ",
+      "id": "1vu8",
+      "visible": false,
+      "value": "0",
+      "variableType": "answer",
+      "isCloud": false,
+      "isRealTime": false,
+      "cloudDate": false,
+      "object": null,
+      "x": 150,
+      "y": -100
+    }
+  ],
+  "messages": [],
+  "functions": [],
+  "tables": [],
+  "speed": 60,
+  "interface": {
+    "menuWidth": 264
+  },
+  "expansionBlocks": [],
+  "aiUtilizeBlocks": [],
+  "hardwareLiteBlocks": [],
+  "externalModules": [],
+  "externalModulesLite": []
+}
