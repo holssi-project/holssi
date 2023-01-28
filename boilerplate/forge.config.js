515f4c770176f879fb721a0aea474bfa98ef6c70
jedeop <jedeop@jedeop.dev>
2023-01-29 01:18:55 +0900

    use electron instead of tauri

---
+++ boilerplate/forge.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+  ],
+};
